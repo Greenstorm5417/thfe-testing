6a6794fd44acef4c6e04a25a8a79b7ea715bde3e
brickmasterhunt <brickmasterlogan@icloud.com>
2026-09-13 11:45:08 -0400

    Stop tracking .xmake and build directories

--- build/.gens/rules/plugin.compile_commands.autoupdate.d
+++
@@ -1,9 +0,0 @@
-{
-    files = {
-        "/home/green/Projects/thfe-testing/xmake.lua",
-        "/home/green/Projects/thfe-testing/.xmake/linux/x86_64/xmake.conf"
-    },
-    values = {
-        "src/main.cpp"
-    }
-}
